d2865c8d90f016a42e20c7159906babf96a49910
sabsaback <44085339+sabsaback@users.noreply.github.com>
2019-02-14 11:31:35 +0200

    14.2
    Todo
    JSON parse
    Graph
    Connect parse to dropdown

--- Uppg1_Norrmann_Baeck/src/guipackage/Window.java
+++ Uppg1_Norrmann_Baeck/src/guipackage/Window.java
@@ -26,11 +26,12 @@ public class Window extends JFrame {
 	
 	
 	
-	private final JSplitPane splitPane;  // split the window in top and bottom
-    private final JPanel upperRight;       // container panel for the top
-    private final JPanel upperLeft;    // container panel for the bottom
-    private final JPanel downRight;
-    private final JPanel downLeft;
+	private final JSplitPane splitPane;  	// split the window in right and left
+	private final JSplitPane leftSplitPane;
+    private final JPanel upperRight;      	// container panel for upper right side
+    private final JPanel upperLeft;    		// container panel for upper left side
+    private final JPanel downRight; 		// container panel for upper right side
+    private final JPanel downLeft;			// container panel for down left side
     //private final JScrollPane scrollPane; // makes the text scrollable
     //private final JTextArea textArea;     // the text
     //private final JPanel inputPanel;      // under the text a container for all the input elements
@@ -40,6 +41,7 @@ public class Window extends JFrame {
 	public Window(){
 		
 		splitPane = new JSplitPane();
+		leftSplitPane = new JSplitPane();
 		
 		
 		upperLeft = new JPanel(new GridBagLayout());
@@ -54,38 +56,42 @@ public class Window extends JFrame {
 		//splitPane.add(downRight);
 		
 		
-		downLeft = new JPanel(new GridBagLayout());
+		downLeft = new JPanel();
 		//splitPane.add(downLeft);
 		
 		
 		this.setLayout(new GridLayout());
 		this.add(splitPane);
 		
-		splitPane.setOrientation(JSplitPane.HORIZONTAL_SPLIT);  // we want it to split the window vertically
-        splitPane.setDividerLocation(400);                    // the initial position of the divider is 400 (our window is 800 pixels high)
-        splitPane.setTopComponent(upperLeft);                  // at the top we want our "topPanel"
+
+		leftSplitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);  // we want it to split the window horizontally
+		leftSplitPane.setDividerLocation(300);                    	// the initial position of the divider is 400 (our window is 800 pixels high)
+		leftSplitPane.setTopComponent(upperLeft);                  	// at the top we want our "topPanel"
+		leftSplitPane.setBottomComponent(downLeft);
+		
+		
+		splitPane.setOrientation(JSplitPane.HORIZONTAL_SPLIT);  // we want it to split the window horizontally
+        splitPane.setDividerLocation(400);                    	// the initial position of the divider is 400 (our window is 800 pixels high)
+        splitPane.setTopComponent(leftSplitPane);                  	// at the top we want our "topPanel"
         splitPane.setBottomComponent(upperRight);
 		
 		this.setTitle("My graph");
-		this.setSize(800, 400);
-		this.setResizable(true);
-		this.setVisible(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(1200, 800);
+		this.setResizable(true);
+		
 		this.setLocationRelativeTo(null);
 		
-		JButton button1 = new JButton("--- Do query ---");
+		JButton button1 = new JButton("--- Do query ---");		//new button
 		
-		JComboBox<Object> dropDown1 = new JComboBox<Object>();
+		JComboBox<Object> dropDown1 = new JComboBox<Object>();	//5 dropdownboxes
 		JComboBox<Object> dropDown2 = new JComboBox<Object>();
 		JComboBox<Object> dropDown3 = new JComboBox<Object>();
 		JComboBox<Object> dropDown4 = new JComboBox<Object>();
 		JComboBox<Object> dropDown5 = new JComboBox<Object>();
 		
 		
-		
-		
-		
-		button1.addActionListener(new ActionListener(){
+		button1.addActionListener(new ActionListener(){			//settings for what happens when we push "Do query"-button (button1)
 			int size = 0;
 
 			@Override
@@ -95,15 +101,10 @@ public class Window extends JFrame {
 			}	
 		});
 		
-		
-		//button1.setBounds(1, 1, 14, 4);
-		
-		
-		
-		
 		GridBagConstraints c = new GridBagConstraints();
 		c.anchor = GridBagConstraints.FIRST_LINE_START;
 		c.weightx = 0.5;
+		//c.fill = GridBagConstraints.NORTH;
 		
 
 		c.gridx = 0;
@@ -134,48 +135,46 @@ public class Window extends JFrame {
 		
 		
 
-		c.fill = GridBagConstraints.VERTICAL;
+		
 		c.gridx = 1;
 		c.gridy = 0;
 		upperLeft.add(dropDown1, c);
 		
-		c.fill = GridBagConstraints.VERTICAL;
 		c.gridx = 1;
 		c.gridy = 1;
 		upperLeft.add(dropDown2, c);
 		
-		c.fill = GridBagConstraints.VERTICAL;
 		c.gridx = 1;
 		c.gridy = 2;
 		upperLeft.add(dropDown3, c);
 		
-		c.fill = GridBagConstraints.VERTICAL;
 		c.gridx = 1;
 		c.gridy = 3;
 		upperLeft.add(dropDown4, c);
 		
-		c.fill = GridBagConstraints.VERTICAL;
 		c.gridx = 1;
 		c.gridy = 4;
 		upperLeft.add(dropDown5, c);
 
-		c.fill = GridBagConstraints.VERTICAL;
 		c.gridx = 1;
 		c.gridy = 5;
 		upperLeft.add(button1, c);
 		
-		
-		this.pack();
-		
-		//this.add(button1);
-		
-		JTextArea textArea = new JTextArea (2,2);
-		//JScrollPane scroll = new JScrollPane(textArea);
+	
+		JTextArea textArea = new JTextArea (25,33);
+		JScrollPane scroll = new JScrollPane(textArea);
 		textArea.setEditable(false);
-		//this.add(textArea);
-		//this.add(scroll);
+		textArea.append("HEJ\nDIDOIJ\n\n\n\nasdasda\n\n\n\n\n\nasdasd\n\n cjodicjdsoc\ndcndn\nf\nfeijwf\nfeif\njoewjf\nidhw\njdoi\ndjwd\ndjow\nHej");
+		
+		//downLeft.add(textArea);
+		
+		downLeft.add(scroll,c);
 		
 		
+		
+		this.setVisible(true);
+		
+	//	this.pack();
 	}
 }
 
